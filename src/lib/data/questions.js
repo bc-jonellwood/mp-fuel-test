6fa1bc8d4ad18458c1c3fbed87c14b30190830df
Jon Ellwood <jon.ellwood@berkeleycountysc.gov>
2024-12-30 08:33:39 -0500

    init commit

---
+++ src/lib/data/questions.js
@@ -0,0 +1,80 @@
+export const questions = [
+	{
+		index: 0,
+		question: 'The State Fleet card is used to purchase:',
+		choices: ['Fuel and Oil', 'Snacks', 'Fuel Only'],
+		correctAnswer: 'Fuel Only',
+		manualSection: '0.0.0'
+	},
+	{
+		index: 1,
+		question: 'The State Fleet card can be used in:',
+		choices: ['Personal Vehicle', 'County Vehicle', 'Both'],
+		correctAnswer: 'Both',
+		manualSection: '0.0.0'
+	},
+	{
+		index: 2,
+		question: 'All State Fleet receipts should provide:',
+		choices: [
+			'Vehicle and Tag Number, office or agency head',
+			'Vendor Name/Address',
+			'Cost/Gallons',
+			'All of these'
+		],
+		correctAnswer: 'All of these',
+		manualSection: '0.0.0'
+	},
+	{
+		index: 3,
+		question: 'How many days do you have to dispute a transaction from the date of purchase?',
+		choices: ['15 Days', '30 Days', '45 Days'],
+		correctAnswer: '15 Days',
+		manualSection: '0.0.0'
+	},
+	{
+		index: 4,
+		question: 'The State Fleet user must turn their receipts in to their:',
+		choices: ['Department Liaison', 'Finance', 'Procurement Director'],
+		correctAnswer: 'Department Liaison',
+		manualSection: '0.0.0'
+	},
+	{
+		index: 5,
+		question:
+			'The State Fleet card should be used as a secondary use to purchase fuel. The County fuel system is always the primary.',
+		choices: ['True', 'False'],
+		correctAnswer: 'True',
+		manualSection: '0.0.0'
+	},
+	{
+		index: 6,
+		question: 'The State Fleet card can be used for personal use.',
+		choices: ['True', 'False'],
+		correctAnswer: 'False',
+		manualSection: '0.0.0'
+	},
+	{
+		index: 7,
+		question:
+			'If your fuel card is misplaced, lost or stolen contact the Fuel Card Administrator in the Fleet Management Dept.',
+		choices: ['True', 'False'],
+		correctAnswer: 'True',
+		manualSection: '0.0.0'
+	},
+	{
+		index: 8,
+		question:
+			'If a receipt is lost or misplaced; the user must write a detailed memo stating all information from the transaction (location purchased, gallons, cost, vehicle number and tag number).',
+		choices: ['True', 'False'],
+		correctAnswer: 'True',
+		manualSection: '0.0.0'
+	},
+	{
+		index: 9,
+		question: 'If purchasing unleaded fuel, mid-grade or premium is okay.',
+		choices: ['True', 'False'],
+		correctAnswer: 'False',
+		manualSection: '0.0.0'
+	}
+];
